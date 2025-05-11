7e12df79f434f9e4d1c9960305eb0ec4dab5dd89
AmahiaPaez <amahiapaez1304@gmail.com>
2025-05-11 00:42:15 -0300

    indentacion

--- Gato.java
+++ Gato.java
@@ -1,5 +1,5 @@
 public  class Gato{
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         int gato1 = 3;
         int gato2 = 4;
         int resultado = gato1 + gato2;
