1e491a2391fbecaa42343f700f5f3069b2ba747d
AmahiaPaez <amahiapaez1304@gmail.com>
2025-05-11 00:37:46 -0300

    añadiendo java

---
+++ Gato.java
@@ -0,0 +1,9 @@
+public  class Gato{
+     public static void main(String[] args) {
+        int gato1 = 3;
+        int gato2 = 4;
+        int resultado = gato1 + gato2;
+        System.out.println("Resultado: "+ resultado);
+    }
+
+}
