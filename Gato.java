69fe547af0858ae072e5695bcd44360d99068a43
AmahiaPaez <amahiapaez1304@gmail.com>
2025-05-11 00:45:46 -0300

    resta

--- Gato.java
+++ Gato.java
@@ -4,6 +4,11 @@ public  class Gato{
         int gato2 = 4;
         int resultado = gato1 + gato2;
         System.out.println("Resultado: "+ resultado);
+
+        int num1 = 4;
+        int num2= 5;
+        int resultado2 = num2 - num1;
+        System.out.println("Resultado:"+ resultado2);
     }
 
 }
